b8a949f1b29f666be25d1bc814ded693a1f87bbc
Vassilis Pontis <vpontis15@gmail.com>
2024-01-25 20:07:02 +0200

    add Like Model

--- blog/models.py
+++ blog/models.py
@@ -5,22 +5,21 @@ from django.utils.text import slugify
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
-
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return f'{self.name}'
-    
 
 
 class Navigation(models.Model):
-    text= models.CharField(max_length=100)
-    destination= models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
 
     def __str__(self):
         return f"Text={self.text}, Destination = {self.destination}"
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -28,12 +27,13 @@ class Post(models.Model):
     body = RichTextUploadingField(blank=True, null=True)
     created_at = models.DateField(auto_now=True)
     tags = models.ManyToManyField(Tag)
-    slug = models.SlugField(unique=True, editable= False, blank= True, db_index=True, default='')
+    slug = models.SlugField(unique=True, editable=False,
+                            blank=True, db_index=True, default='')
     comments = models.ManyToManyField('Comment', blank=True)
+    likes = models.ManyToManyField('Like', blank=True)
 
     class Meta:
         ordering = ['-created_at']
-
 
     def save(self, *args, **kwargs):
 
@@ -42,22 +42,26 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Title = {self.title} Author = {self.author} Content = {self.body[:50]} Date = {self.created_at} Tags = {self.tags}"
-    
-
-
-
-    
 
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post_to_comment = models.ForeignKey(Post, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(default=timezone.now)
-    body = models.TextField();
+    body = models.TextField()
 
     class Meta:
         ordering = ['-timestamp']
 
     def __str__(self):
-        return f"User: {self.user} Post: {self. post_to_comment.title} Body: {self.body}"
+        return f"User: {self.user} commented  {self.body} on {self. post_to_comment.title} "
 
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    liked_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+
+def __str__(self):
+    return f'{self.user.username} liked {self.content_object}'
